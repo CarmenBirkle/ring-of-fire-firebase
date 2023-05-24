32e8215c482498410afb16a6685af16a30a8bb56
Carmen Birkle <carmen.birkle@iu-study.org>
2023-05-24 13:34:31 +0200

    firebase connection

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-2dd0f',
+    appId: '1:199553057180:web:d07364a44786ca918e4859',
+    storageBucket: 'ring-of-fire-2dd0f.appspot.com',
+    apiKey: 'AIzaSyBKe7zJpO-8UgLgPpLjw0tjFA6DTg7EbQA',
+    authDomain: 'ring-of-fire-2dd0f.firebaseapp.com',
+    messagingSenderId: '199553057180',
+  },};
