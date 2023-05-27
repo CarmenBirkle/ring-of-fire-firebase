ecd4d97071f8ac547daa9fac315dda46232fe1e3
Carmen Birkle <carmen.birkle@iu-study.org>
2023-05-27 19:50:51 +0200

    favicon and scrollbar

--- src/models/game.ts
+++ src/models/game.ts
@@ -36,18 +36,12 @@ function shuffle(array: any) {
   var currentIndex = array.length,
     temporaryValue,
     randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
+    while (0 !== currentIndex) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
-
-    // And swap it with the current element.
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
   }
-
   return array;
 }
